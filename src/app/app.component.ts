002572e3e0bd0876cda8906502505031fd3c0c10
Chen Wei <weichen2046@gmail.com>
2017-01-04 00:29:18 +0800

    Demo for remote.BrowserWindow.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 
+import { remote } from 'electron';
+
+let {BrowserWindow} = remote;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app works!';
-
   constructor(
   ) { }
 
   ngOnInit() {
+  }
+
+  public newWindow() {
+    let newW = new BrowserWindow({width: 1200, height: 600});
+    newW.loadURL('https://www.google.com.hk');
+    newW.on('close', () => {newW = null});
+    newW.show();
   }
 }
